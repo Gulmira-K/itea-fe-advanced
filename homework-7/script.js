e23861c7c4d6e9bdab6ebc5da3ff4fc1593b23b0
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-08-06 08:51:57 +0200

    solved last 2 exercises

--- homework-7/script.js
+++ homework-7/script.js
@@ -7,7 +7,7 @@ function binaryAgent(str) {
     return String.fromCharCode(parseInt(binary, 2));
   });
 
-  return lettersArray.join(' ');
+  return lettersArray.join('');
 }
 
 console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
@@ -64,3 +64,74 @@ console.log(addTogether(2, 3))
 // Run the tests to see the expected output for each method. 
 // The methods that take an argument must accept only one argument and it has to be a string.
 // These methods must be the only available means of interacting with the object.
+
+let Person = function (firstAndLast) {
+  let fullName = firstAndLast;
+
+  this.getFirstName = function () {
+    return  fullName.split(' ')[0];
+  }
+
+   this.getLastName = function () {
+     return  fullName.split(' ')[1];
+  }
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function (first) {
+    return fullName = `${first} ${fullName.split(' ')[1]}`;
+  }
+
+  this.setLastName = function (last) {
+    return fullName = `${fullName.split(' ')[0]} ${last} `;
+  }
+
+  this.setFullName = function (firstAndLast) {
+    return fullName = firstAndLast;
+  }
+
+  return firstAndLast;
+};
+
+const bob = new Person('Bob Ross');
+console.log(bob.getFullName());
+console.log(bob.setFirstName("Haskell"));
+console.log(bob.setLastName("Curry"));
+console.log(bob.getFullName());
+
+// Map the Debris
+// Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+// The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+// You can read about orbital periods on Wikipedia.
+// The values should be rounded to the nearest whole number. The body being orbited is Earth.
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+// T = 2PI * SQRT[R^3 / GM]
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418,
+        earthRadius = 6367.4447;
+  
+  let R,
+      T;
+  
+  arr = arr.map(elem => {
+    if (elem.hasOwnProperty('avgAlt')) {
+      R = earthRadius + elem['avgAlt'];
+      T = Math.round((Math.PI * 2) * (Math.sqrt(Math.pow(R, 3) / GM)));
+
+      elem['orbitalPeriod'] = elem['avgAlt'];
+      delete elem['avgAlt'];
+      
+      elem['orbitalPeriod'] = T;
+      
+      return elem;
+    }
+  })
+
+  return arr;
+}
+
+console.log(orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]));
