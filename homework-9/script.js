245abeb331fc4ab943557e44450bc3bf86e45bcc
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-08-13 18:02:25 +0200

    transform to roman numbers exercise solution

--- homework-9/script.js
+++ homework-9/script.js
@@ -21,3 +21,25 @@ console.log(palindrome("almostomla"));
 console.log(palindrome("1 eye for of 1 eye."));
 console.log(palindrome("five|\_/|four")); 
 
+
+// Roman Numeral Converter
+// Convert the given number into a roman numeral.
+// All roman numerals answers should be provided in upper-case.
+
+function convertToRoman(num) {
+  let result = '';
+
+  const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+        decimals.map(function (decimal, index) {
+          while (num >= decimal) {
+            result += roman[index];
+            num -= decimal;
+          }
+        });
+
+ return result;
+}
+
+console.log(convertToRoman(36));
